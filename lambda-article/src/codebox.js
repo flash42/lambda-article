4f017f34f2a8637db6f990efa334376ab351a796
Peresztegi Adam <flash42@gmail.com>
2022-09-21 16:30:08 +0200

    WIP Add Codemirror with custom json syntax highlighter
    - Add minimal integration of Codemirror
    - Inline json language module and parser to play with it
    - HACK: run leser generator before npm start
    - Syntax highlight 'strings' (not valid JSON)

---
+++ lambda-article/src/codebox.js
@@ -0,0 +1,50 @@
+import CodeMirror from '@uiw/react-codemirror';
+import { json } from './lang-lambda/json';
+// import { json } from '@codemirror/lang-json';
+import React from 'react';
+
+
+const starterCode = `{
+    "firstName": 'John',
+    "lastName": "Smith",
+    "isAlive": true,
+    "age": 27,
+    "address": {
+      "streetAddress": "21 2nd Street",
+      "city": "New York",
+      "state": "NY",
+      "postalCode": "10021-3100"
+    },
+    "phoneNumbers": [
+      {
+        "type": "home",
+        "number": "212 555-1234"
+      },
+      {
+        "type": "office",
+        "number": "646 555-4567"
+      }
+    ],
+    "children": [
+        "Catherine",
+        "Thomas",
+        "Trevor"
+    ],
+    "spouse": null
+  }`;
+
+function Codebox() {
+    const onChange = React.useCallback((value, _) => {
+        console.log('value:', value);
+      }, []);
+    return (
+        <CodeMirror
+        value={starterCode}
+        height="200px"
+        extensions={[json({ jsx: true })]}
+        onChange={onChange}
+      />
+    );
+  }
+  
+  export default Codebox;
